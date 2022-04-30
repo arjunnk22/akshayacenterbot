079bb8dff47bf205aa75a2a46dc67c77bd3ed92e
deadXcriminal <arjunnknair@gmail.com>
2022-04-30 21:01:37 +0530

    updtmain

---
+++ main.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const { Client, Intents, Collection } = require("discord.js");
+
+const FiveM = require("fivem") // Import the npm package.
+
+
+const srv = new FiveM.Server('34.93.243.223:30120') // Set the IP with port.
+
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS
+    ]
+});
+
+
+client.once('ready', () => {
+
+    
+    console.log("Bot is Online");
+    setInterval(() => {
+        srv.getPlayers().then((result) => {
+            client.user.setActivity(`with ${result} players`,{ type: 'PLAYING' });
+        })
+        .catch(function () {
+            client.user.setActivity("AIVPV4",{ type: 'WATCHING' });
+        })
+      }, 10000);
+});
+
+
+
+
+client.login(process.env.TOKEN);
